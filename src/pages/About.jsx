0d08e0813a38cb1e8fa814a957628e7ee139e81a
jannat-349 <jucse28.349@gmail.com>
2024-08-10 21:16:25 +0600

    About section has been polished

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,51 +1,75 @@
 import React from 'react';
 import '../styles/About.css';
 
+
 const About = () => {
   return (
     <div className="about-container">
       <section>
         <h2>Personal Information</h2>
-        <div className="personal-info">
-          <div className="personal-info-item">
-            <h3>Name:</h3>
-            <p>Jannatul Ferdoush Jannati</p>
+        <div className="personal-info-container">
+          <div className="personal-info">
+            <div className="personal-info-item">
+              <h3>Name: </h3>
+              <p>Jannatul Ferdoush Jannati</p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Date of Birth: </h3>
+              <p>February 18, 2000</p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Father's Name: </h3>
+              <p>Md. Ali Akbar</p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Mother's Name: </h3>
+              <p>Tahmina Akter</p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Age: </h3>
+              <p>24</p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Gender: </h3>
+              <p>Female</p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Nationality: </h3>
+              <p>Bangladeshi</p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Religion: </h3>
+              <p>Islam</p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Hobbies: </h3>
+              <p>Reading, Cycling, Cooking, Crafting, Traveling, Singing, Gardening, Abstract Drawing</p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Skills: </h3>
+              <br />
+              <p>
+                <strong>Languages:</strong> Python, C/C++, JavaScript, HTML/CSS, Bootstrap <br />
+                <strong>Frameworks and Database:</strong> React, NodeJS, WordPress, Material-UI, Postman, SQL, MongoDB <br />
+                <strong>Developer Tools:</strong> Git & Github, Docker, VS Code, Google Colab <br />
+                <strong>Libraries:</strong> mongoose, unittest, JWToken, pandas, numpy, matplotlib
+              </p>
+            </div>
+            <div className="personal-info-item">
+              <h3>Blood Group: </h3>
+              <p>O+</p>
+            </div>
           </div>
-          <div className="personal-info-item">
-            <h3>Date of Birth:</h3>
-            <p>February 18, 2000</p>
-          </div>
-          <div className="personal-info-item">
-            <h3>Father's Name:</h3>
-            <p>Md. Ali Akbar</p>
-          </div>
-          <div className="personal-info-item">
-            <h3>Mother's Name:</h3>
-            <p>Tahmina Akter</p>
-          </div>
-          <div className="personal-info-item">
-            <h3>Age:</h3>
-            <p>24</p>
-          </div>
-          <div className="personal-info-item">
-            <h3>Gender:</h3>
-            <p>Female</p>
-          </div>
-          <div className="personal-info-item">
-            <h3>Nationality:</h3>
-            <p>Bangladeshi</p>
-          </div>
-          <div className="personal-info-item">
-            <h3>Religion:</h3>
-            <p>Islam</p>
-          </div>
-          <div className="personal-info-item">
-            <h3>Hobbies:</h3>
-            <p>Coding, Reading, Traveling</p>
-          </div>
-          <div className="personal-info-item">
-            <h3>Skills:</h3>
-            <p>React, JavaScript, CSS, SQA</p>
+
+          <div className="images-container">
+            <div className="image-item">
+              <img src="src/assets/images/day.jpeg" alt="What I do at day" />
+              <p><em>What I do during the day</em></p>
+            </div>
+            <div className="image-item">
+              <img src="src/assets/images/night.jpg" alt="What I do at night" />
+              <p><em>What I do at night</em></p>
+            </div>
           </div>
         </div>
       </section>
@@ -54,13 +78,12 @@ const About = () => {
         <h2>Experience</h2>
         <div className="experience-item">
           <div className="experience-header">
-            <h3>
-              Junior SQA Engineer
-            </h3>
+            <h3>Junior SQA Engineer</h3>
             <p className="duration">April 2024 -- Present</p>
           </div>
           <a href="https://www.dsinnovators.com/" target="_blank" rel="noopener noreferrer">
-            <p>Dynamic Solution Innovators Ltd.</p></a>
+            <p>Dynamic Solution Innovators Ltd.</p>
+          </a>
           <ul>
             <li>Ensured quality of a Bangladeshi government project named IPEMIS under DPE, Bangladesh.</li>
             <li>Completed training on Software Quality Assurance organized by the company.</li>
@@ -68,13 +91,12 @@ const About = () => {
         </div>
         <div className="experience-item">
           <div className="experience-header">
-            <h3>
-              MERN Development Intern
-            </h3>
+            <h3>MERN Development Intern</h3>
             <p className="duration">March 2023 -- September 2023</p>
           </div>
           <a href="https://bongodev.com/" target="_blank" rel="noopener noreferrer">
-            <p>bongoDev</p></a>
+            <p>bongoDev</p>
+          </a>
           <ul>
             <li>Developed Employee Management, Pagination, E-commerce projects.</li>
             <li>Reviewed codebase and tasks of students at bongoDev.</li>
@@ -84,13 +106,12 @@ const About = () => {
         </div>
         <div className="experience-item">
           <div className="experience-header">
-            <h3>
-              Executive Member
-            </h3>
+            <h3>Executive Member</h3>
             <p className="duration">Jan 2023 - April 2024</p>
           </div>
           <a href="https://jurc.org/" target="_blank" rel="noopener noreferrer">
-            <p>Jahangirnagar University Robotics Club</p></a>
+            <p>Jahangirnagar University Robotics Club</p>
+          </a>
           <ul>
             <li>Implemented Line Following Robot, Wirebuzz Game.</li>
             <li>Participated in Robotics competitions held at IUBAT and CU.</li>
@@ -98,6 +119,7 @@ const About = () => {
           </ul>
         </div>
       </section>
+
       <section>
         <h2>Education</h2>
         <div className="education-item">

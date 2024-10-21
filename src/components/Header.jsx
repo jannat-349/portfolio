1602c902f9e1de917aa06174468aa5b481e68ce5
jannatul-dsi <jannatul.ferdoush@dsinnovators.com>
2024-10-21 15:23:20 +0600

    Fixed the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/images/my_photo.jpg';
 import '../styles/Header.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import IconButton from '@mui/material/IconButton';
@@ -29,15 +28,15 @@ const Header = () => {
 
     return (
         <header className={`header ${scrolled ? 'scrolled' : ''}`}>
-            <Link to="/">
-                <img src={logo} alt="Logo" className="logo" />
-            </Link>
+            <div className="logo-container">
+                <Link to={"/"} className="fancy-name">Jannatul Ferdoush Jannati</Link>
+            </div>
             <nav className={`nav-menu ${menuOpen ? 'active' : ''}`}>
-                <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}><i className="fas fa-home"></i> Home</Link>
-                <Link to="/about" className="nav-link" onClick={() => setMenuOpen(false)}><i className="fas fa-user"></i> About</Link>
-                <Link to="/projects" className="nav-link" onClick={() => setMenuOpen(false)}><i className="fas fa-briefcase"></i> Projects</Link>
-                <Link to="/researches" className="nav-link" onClick={() => setMenuOpen(false)}><i className="fas fa-file-alt"></i> Researches</Link>
-                <Link to="/achievements" className="nav-link" onClick={() => setMenuOpen(false)}><i className="fas fa-trophy"></i> Achievements</Link>
+                <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}> Home</Link>
+                <Link to="/about" className="nav-link" onClick={() => setMenuOpen(false)}> About</Link>
+                <Link to="/projects" className="nav-link" onClick={() => setMenuOpen(false)}> Projects</Link>
+                <Link to="/researches" className="nav-link" onClick={() => setMenuOpen(false)}> Researches</Link>
+                <Link to="/achievements" className="nav-link" onClick={() => setMenuOpen(false)}> Achievements</Link>
             </nav>
             <IconButton
                 edge="end"

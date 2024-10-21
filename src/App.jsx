a5ba0a629f71e968055a421ef5c332c2d8b0031b
jannatul-dsi <jannatul.ferdoush@dsinnovators.com>
2024-10-21 16:28:34 +0600

    Fixed the education page

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,14 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import Header from './components/Header';
-import Footer from './components/Footer';
 
 import Home from './pages/Home';
-import About from './pages/About';
 import Projects from './pages/Projects';
 import Researches from './pages/Researches';
 import Achievements from './pages/Achievements';
 import ScrollToTop from './components/ScrollToTop';
 import ErrorPage from './components/ErrorPage';
+import Education from './pages/Education';
 
 
 const App = () => {
@@ -24,7 +23,7 @@ const App = () => {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/education" element={<Education />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/researches" element={<Researches />} />
         <Route path="/achievements" element={<Achievements />} />
---
+++ src/pages/Education.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../styles/About.css';
+
+
+const Education = () => {
+  return (
+    <div className="about-container">
+      <section>
+        <h2>Education</h2>
+        <div className="education-item">
+          <div className="education-header">
+            <h3>
+              <a href="https://www.juniv.edu" target="_blank" rel="noopener noreferrer">
+                Jahangirnagar University
+              </a>
+            </h3>
+            <p className="duration">Feb. 2019 -- Apr. 2024</p>
+          </div>
+          <p>Bachelor of Science in Computer Science and Engineering (CGPA: 3.84, Position: 3/59)</p>
+        </div>
+        <div className="education-item">
+          <div className="education-header">
+            <h3>
+              <a href="https://www.hcc.edu.bd/" target="_blank" rel="noopener noreferrer">
+                Holy Cross College
+              </a>
+            </h3>
+            <p className="duration">2016 -- 2018</p>
+          </div>
+          <p>Science (GPA: 5.00/5.00)</p>
+        </div>
+        <div className="education-item">
+          <div className="education-header">
+            <h3>
+              <a href="https://www.facebook.com/bbahschool/" target="_blank" rel="noopener noreferrer">
+                Bangladesh Bank Adarsha High School
+              </a>
+            </h3>
+            <p className="duration">2006 -- 2016</p>
+          </div>
+          <p>Science (GPA: 5.00/5.00)</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Education;

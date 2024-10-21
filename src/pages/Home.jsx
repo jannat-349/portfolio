c7c6a192e086c82efce394c8cd6178bbab5b88b5
jannatul-dsi <jannatul.ferdoush@dsinnovators.com>
2024-10-21 16:21:30 +0600

    Fixed the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,49 @@
 import React from 'react';
-import Carousel from '../components/Carousel';
-import '../styles/Home.css';
 import { Link } from 'react-router-dom';
+import '../styles/Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faGoogle, faOrcid } from '@fortawesome/free-brands-svg-icons';
+import profileImage from '../assets/images/my image.jpg';
 
 const Home = () => {
     return (
         <div className="home">
-            <Carousel />
-            <div className="overlay">
-                <main className="main-content">
+            <div className="content-container">
+                <div className="left-section">
+                    <img 
+                        src={profileImage}  
+                        alt="Jannatul's photo"
+                        className="profile-photo" 
+                    />
+                    <div className="social-links">
+                        <a href="https://www.linkedin.com/in/jannat349/" className="social-icon" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                        <a href="https://github.com/jannat-349" className="social-icon" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                        <a href="https://scholar.google.com/citations?user=G_z0QKQAAAAJ&hl=en" className="social-icon" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGoogle} />
+                        </a>
+                        <a href="https://orcid.org/0009-0002-0053-884X" className="social-icon" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faOrcid} />
+                        </a>
+                    </div>
+                    <div className="email-section">
+                        <p><strong>Email: </strong>jannatul.stu2018@juniv.edu</p>
+                    </div>
+                </div>
+
+                {/* Right Section: Bio */}
+                <div className="right-section">
                     <h1 className="bio">
-                        <span className="bold-white">
-                            Hey!! I'm Jannatul, a passionate learner who enjoys leading in various tasks,
-                            from crafting paper planes 
-                            <i className="fas fa-paper-plane fly-icon"></i>
-                            to programming computers
-                            <i className="fas fa-desktop typing-icon"></i>. <br />  
-                        </span>
-                        <span className="border-white">
-                            I also dream of flying on a plane one day,
-                            just as I enjoy riding my bicycle 
-                            <i className="fas fa-bicycle move-icon"></i>
-                        </span>
+                        Hey! I'm Jannatul, a passionate learner and leader. I enjoy exploring diverse fields from crafting creative projects to programming. I also dream of traveling the world one day, just as I enjoy everyday adventures.
                     </h1>
                     <div className="button-container">
                         <Link to="/about" className="btn btn-primary">More About Me</Link>
                         <Link to="/researches" className="btn btn-secondary">My Research Works</Link>
                     </div>
-                </main>
+                </div>
             </div>
         </div>
     );

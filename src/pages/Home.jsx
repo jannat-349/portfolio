aed8896e30dae829663f666a995273adea89df7f
jannatul-dsi <jannatul.ferdoush@dsinnovators.com>
2024-10-21 17:08:26 +0600

    Fixed the homepage bio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
         <div className="home">
             <div className="content-container">
                 <div className="left-section">
-                    <img 
-                        src={profileImage}  
+                    <img
+                        src={profileImage}
                         alt="Jannatul's photo"
-                        className="profile-photo" 
+                        className="profile-photo"
                     />
                     <div className="social-links">
                         <a href="https://www.linkedin.com/in/jannat349/" className="social-icon" target="_blank" rel="noopener noreferrer">
@@ -37,10 +37,17 @@ const Home = () => {
                 {/* Right Section: Bio */}
                 <div className="right-section">
                     <h1 className="bio">
-                        Hey! I'm Jannatul, a passionate learner and leader. I enjoy exploring diverse fields from crafting creative projects to programming. I also dream of traveling the world one day, just as I enjoy everyday adventures.
+                        I am Jannatul, a dedicated and driven professional with a strong focus on technology and continuous learning. Currently, I am employed as an SQA Engineer at <a href="https://www.dsinnovators.com" target="_blank" rel="noopener noreferrer">Dynamic Solution Innovators Ltd.</a>, where I contribute to enhancing software quality and ensuring the delivery of efficient and reliable systems.
+                        <br></br>
+                        <br></br>
+                        I hold a undergraduate degree from Jahangirnagar University, where I cultivated my expertise in computer science and engineering. My academic background has provided me with a solid foundation in areas such as data analysis, problem-solving, and innovative research approaches.
+                        <br></br>
+                        <br></br>
+                        My research interests span a range of cutting-edge technologies, including Artificial Intelligence (AI), Machine Learning (ML), Graph Neural Networks (GNN), and Robotics. I am deeply passionate about exploring these fields and their potential to drive innovation and solve real-world challenges.
                     </h1>
+
                     <div className="button-container">
-                        <Link to="/about" className="btn btn-primary">More About Me</Link>
+                        <Link to="/projects" className="btn btn-primary">My Projects</Link>
                         <Link to="/researches" className="btn btn-secondary">My Research Works</Link>
                     </div>
                 </div>
